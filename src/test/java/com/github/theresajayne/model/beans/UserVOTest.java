cef392177e32cf825c7a5fc0bee7ee5a4b4d627e
theresajayne <theresajayne@minuet-uk.com>
2013-05-03 20:16:07 +0100

    Added Spring to Maven Pom

--- src/test/java/com/github/theresajayne/model/beans/UserVOTest.java
+++ src/test/java/com/github/theresajayne/model/beans/UserVOTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static junit.framework.Assert.assertEquals;
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 
 /**

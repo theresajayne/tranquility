c191a6284d984761d87c5de4f33061e75c1fa71e
Theresa Forster <theresajayne@minuet-uk.com>
2013-07-22 15:58:04 +0100

    Update ConverterImpl.java

--- src/main/java/com/github/theresajayne/tranquility/common/ConverterImpl.java
+++ src/main/java/com/github/theresajayne/tranquility/common/ConverterImpl.java
@@ -36,18 +36,12 @@ public class ConverterImpl<FB extends FormBean, VO extends ValueObject> implemen
         } catch (Exception e) {
             LOG.error(e);
         } finally {
-            //Copy nested Form Bean to Value Object
+            
             copyNestedFB(fb, vo);
         }
         return vo;
     }
 
-    /**
-     * Returns form bean of value object with copied properties including nested properties.
-     *
-     * @param vo - An object of type ValueObject
-     * @return fb - An object of type FormBean
-     */
     public FB toFormBean(VO vo) {
         if (vo == null) return null;
         String fbName = getName(vo);
@@ -60,8 +54,7 @@ public class ConverterImpl<FB extends FormBean, VO extends ValueObject> implemen
         } catch (Exception e) {
             LOG.error(e);
         } finally {
-            //Copy nested Form Bean to Value Object
-            copyNestedVO(vo, fb);
+           copyNestedVO(vo, fb);
         }
         return fb;
     }
@@ -77,9 +70,7 @@ public class ConverterImpl<FB extends FormBean, VO extends ValueObject> implemen
                     target.on(fb).invoke().setterFor(field).withValue(fieldValue);
                 }
             } catch (Exception e) {
-                //Don't care exception as we are trying to copy of <ValueObject> type object into <FormBean>
-                //So this method will definitely throw exception, so catch the exception and copy other fields
-                //LOG.warn(e);
+ 
             }
         }
     }
@@ -93,37 +84,34 @@ public class ConverterImpl<FB extends FormBean, VO extends ValueObject> implemen
     }
 
     private void nestedValueObject(VO vo, FB fb) throws Exception {
-        //All Value Object fields
+       
         Field[] fields = vo.getClass().getDeclaredFields();
 
         for (Field voField : fields) {
-            //Filter on Value Object filed
+            
             if (ValueObject.class.isAssignableFrom(voField.getType())) {
-                //Get nested Value Object
+                
                 @SuppressWarnings("unchecked")
                 VO nestedVO = (VO) PropertyUtils.getNestedProperty(vo, voField.getName());
                 if (nestedVO != null) {
-                    //Get nested Form Bean name from nested Value Object
+                    
                     String nestedFbName = getName(nestedVO);
-                    //Instantiate nested Form Bean class
+                    
                     @SuppressWarnings("unchecked")
                     Class<FB> nestedFbClass = (Class<FB>) Class.forName(nestedFbName);
-                    //Instantiate nested equivalent Form Bean
+                    
                     @SuppressWarnings("unchecked")
                     FB nestedF = nestedFbClass.newInstance();
-                    //Populate nested form bean with nested Value object
+                    
                     PropertyUtils.copyProperties(nestedF, nestedVO);
 
-                    //Set nested property in parent value object
                     try {
-                        //When fields name in both <FormBean> and <ValueObject> are same.
+                        
                         new Mirror().on(fb).invoke().setterFor(voField).withValue(nestedF);
                     } catch (Exception e) {
-                        //When fields name in both <FormBean> and <ValueObject> are different.
+                        
                         PropertyUtils.setNestedProperty(fb, StringUtils.uncapitalize(StringUtils.substringAfterLast(nestedFbName, Constants.CONVERTER_DOT)), nestedF);
                     }
-
-                    //Recursive call until nested form beans over
                     nestedValueObject(nestedVO, nestedF);
                 }
             }
@@ -132,7 +120,7 @@ public class ConverterImpl<FB extends FormBean, VO extends ValueObject> implemen
     }
 
     private void nestedValueObjectCollectionType(VO vo, FB fb, Field voField) throws Exception {
-        //If fields are of type Collection<ValueObject>
+       
         if (Collection.class.isAssignableFrom(voField.getType()) && ValueObject.class.isAssignableFrom(getFieldGenericType(voField))) {
             @SuppressWarnings("unchecked")
             List<VO> listOfVos = (List<VO>) new Mirror().on(vo).invoke().getterFor(voField);
@@ -154,10 +142,8 @@ public class ConverterImpl<FB extends FormBean, VO extends ValueObject> implemen
                         try {
                             PropertyUtils.copyProperties(FB, VO);
                         } catch (Exception e) {
-                            //Don't care as It has to copy other fields in loop
+                           
                         }
-
-                        //Collection object can have another collection objects of Value Object type
                         nestedValueObject(VO, FB);
 
                         newListOfFbs.add(FB);
@@ -178,36 +164,25 @@ public class ConverterImpl<FB extends FormBean, VO extends ValueObject> implemen
     }
 
     private void nestedFormBeans(FB fb, VO VO) throws Exception {
-        //All form Beans fields
-        Field[] fields = fb.getClass().getDeclaredFields();
+         Field[] fields = fb.getClass().getDeclaredFields();
 
         for (Field fbField : fields) {
-            //Filter on form bean filed
             if (FormBean.class.isAssignableFrom(fbField.getType())) {
-                //Get nested form bean value
                 @SuppressWarnings("unchecked")
                 FB nestedFB = (FB) PropertyUtils.getNestedProperty(fb, fbField.getName());
                 if (nestedFB != null) {
-                    //Get nested Value object name from nested Form Bean
                     String nestedVoName = getName(nestedFB);
-                    //Instantiate nested Value Object class
                     @SuppressWarnings("unchecked")
                     Class<VO> nestedVoClass = (Class<VO>) Class.forName(nestedVoName);
-                    //Create nested equivalent value object
                     VO nestedV = nestedVoClass.newInstance();
-                    //Populate nested Value object with nested form bean
                     PropertyUtils.copyProperties(nestedV, nestedFB);
-                    //Set nested property in parent value object
-
+ 
                     try {
-                        //When fields name in both <FormBean> and <ValueObject> are same.
                         new Mirror().on(VO).invoke().setterFor(fbField).withValue(nestedV);
                     } catch (Exception e) {
-                        //When fields name in both <FormBean> and <ValueObject> are different.
                         PropertyUtils.setNestedProperty(VO, StringUtils.uncapitalize(StringUtils.substringAfterLast(nestedVoName, Constants.CONVERTER_DOT)), nestedV);
                     }
 
-                    //Recursive call until nested form beans over
                     nestedFormBeans(nestedFB, nestedV);
                 }
             }
@@ -216,13 +191,11 @@ public class ConverterImpl<FB extends FormBean, VO extends ValueObject> implemen
     }
 
     private void nestedFormBeanCollectionType(FB fb, VO VO, Field fbField) throws Exception {
-        //If fields are of type Collection<FormBean>
         if (Collection.class.isAssignableFrom(fbField.getType()) && FormBean.class.isAssignableFrom(getFieldGenericType(fbField))) {
             @SuppressWarnings("unchecked")
             Collection<FB> listOfFb = (Collection<FB>) new Mirror().on(fb).invoke().getterFor(fbField);
 
             if (listOfFb != null) {
-                //Create same type of collection class
                 @SuppressWarnings("unchecked")
                 Collection<ValueObject> newListOfVo = listOfFb.getClass().newInstance();
 
@@ -237,10 +210,8 @@ public class ConverterImpl<FB extends FormBean, VO extends ValueObject> implemen
                     try {
                         PropertyUtils.copyProperties(vc, FB);
                     } catch (Exception e) {
-                        //Don't care for exception as It has to copy other fields in loop
                     }
 
-                    //Current collection object can have child collection object of type Value Object
                     nestedFormBeans(FB, vc);
 
                     newListOfVo.add(vc);
@@ -270,9 +241,6 @@ public class ConverterImpl<FB extends FormBean, VO extends ValueObject> implemen
                     target.on(VO).invoke().setterFor(field).withValue(fieldValue);
                 }
             } catch (Exception e) {
-                //Don't care exception as we are trying to copy of <FormBean> type object into <ValueObject>
-                //So this method will definitely throw exception, so catch the exception and copy other fields
-                //LOG.warn(e);
             }
         }
     }
